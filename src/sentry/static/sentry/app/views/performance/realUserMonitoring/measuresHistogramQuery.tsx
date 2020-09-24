4163982243d79fe07560f87f9b1d0a1291467a13
Tony Xiao <txiao@sentry.io>
2020-09-24 13:58:46 -0400

    updated to use the measurements histogram query

--- src/sentry/static/sentry/app/views/performance/realUserMonitoring/measuresHistogramQuery.tsx
+++ src/sentry/static/sentry/app/views/performance/realUserMonitoring/measuresHistogramQuery.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import {Location} from 'history';
 
 import {Organization} from 'app/types';
+import DiscoverQuery from 'app/utils/discover/discoverQuery';
 import EventView from 'app/utils/discover/eventView';
 
-import {NUM_BUCKETS} from './constants';
-import {HistogramData} from './types';
+import {NUM_BUCKETS, WEB_VITAL_DETAILS} from './constants';
+import {HistogramData, WebVital} from './types';
 
 type ChildrenProps = {
   isLoading: boolean;
@@ -18,9 +19,6 @@ type Props = {
   organization: Organization;
   eventView: EventView;
   measures: string[];
-  // TODO(tonyx): remove the min/max this is just for populating test data
-  min: number;
-  max: number;
   children: (props: ChildrenProps) => React.ReactNode;
 };
 
@@ -30,31 +28,79 @@ type Props = {
  * retrieve the true data from the backend.
  */
 class MeasuresQuery extends React.Component<Props> {
-  getHistograms(_x) {
-    const {measures} = this.props;
-    const {min, max} = this.props;
+  generateEventView() {
+    const {eventView} = this.props;
 
-    return measures.reduce((histogram, measure) => {
-      histogram[measure] = Array(NUM_BUCKETS)
-        .fill(null)
-        .map((_, i) => ({
-          histogram: i * ((max - min) / NUM_BUCKETS),
-          count: Math.floor(Math.random() * 100),
-        }));
-      return histogram;
-    }, {});
+    const measurementsJoin = `measurements_join(${Object.values(WebVital).join(',')})`;
+    const measurementsHistogram = `measurements_histogram(${NUM_BUCKETS},null,null,0)`;
+    const orderby = `measurements_histogram_${NUM_BUCKETS}_null_null_0`;
+
+    return EventView.fromSavedQuery({
+      id: '',
+      name: '',
+      version: 2,
+      fields: [measurementsJoin, measurementsHistogram, 'count()'],
+      orderby,
+      projects: eventView.project,
+      range: eventView.statsPeriod,
+      query: eventView.query,
+      environment: eventView.environment,
+      start: eventView.start,
+      end: eventView.end,
+    });
   }
 
   render() {
-    const {children} = this.props;
+    const {children, location, organization} = this.props;
 
-    const histogramResults = null;
+    return (
+      <DiscoverQuery
+        location={location}
+        orgSlug={organization.slug}
+        eventView={this.generateEventView()}
+      >
+        {({isLoading, error, tableData}) => {
+          // TODO(tonyx): Handle the empty states
+          if (isLoading) {
+            return 'loading';
+          } else if (error) {
+            return 'errored';
+          }
 
-    return children({
-      isLoading: false,
-      errors: [],
-      histogram: this.getHistograms(histogramResults),
-    });
+          const measurements = Object.values(WebVital).map(s => s.replace('.', '_'));
+          const measurementsKey = `measurements_join_${measurements.join('_')}`;
+          const binKey = `measurements_histogram_${NUM_BUCKETS}_null_null_0`;
+
+          const SLUG_TO_VITAL = Object.values(WebVital).reduce((mapping, vital) => {
+            mapping[WEB_VITAL_DETAILS[vital].slug] = vital;
+            return mapping;
+          }, {});
+
+          const results = (tableData?.data ?? []).reduce(
+            (res, row) => {
+              // TODO(tonyx): should the backend transform this to the expected format?
+              const measurement = SLUG_TO_VITAL[row[measurementsKey]];
+              const histogram = row[binKey];
+              const count = row.count;
+              res[measurement].push({histogram, count});
+              return res;
+            },
+            {
+              [WebVital.FP]: [],
+              [WebVital.FCP]: [],
+              [WebVital.LCP]: [],
+              [WebVital.FID]: [],
+            }
+          );
+
+          return children({
+            isLoading: false,
+            errors: [],
+            histogram: results,
+          });
+        }}
+      </DiscoverQuery>
+    );
   }
 }
 
