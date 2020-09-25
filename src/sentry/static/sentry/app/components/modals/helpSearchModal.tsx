e547c1714020e069b5746fc39620348653aee744
Evan Purkhiser <evanpurkhiser@gmail.com>
2020-09-24 17:55:32 -0700

    feat(support): Replace algolia search w/ SentryGlobalSearch

---
+++ src/sentry/static/sentry/app/components/modals/helpSearchModal.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import {ClassNames, css} from '@emotion/core';
+
+import {t} from 'app/locale';
+import theme from 'app/utils/theme';
+import {ModalRenderProps} from 'app/actionCreators/modal';
+import HelpSearch from 'app/components/helpSearch';
+import Hook from 'app/components/hook';
+import {Organization} from 'app/types';
+
+type Props = ModalRenderProps & {organization: Organization};
+
+const HelpSearchModal = ({Body, closeModal, organization, ...props}: Props) => (
+  <Body>
+    <ClassNames>
+      {({css: injectedCss}) => (
+        <HelpSearch
+          {...props}
+          entryPoint="sidebar_help"
+          dropdownStyle={injectedCss`
+                width: 100%;
+                border: transparent;
+                border-top-left-radius: 0;
+                border-top-right-radius: 0;
+                position: initial;
+                box-shadow: none;
+                border-top: 1px solid ${theme.borderLight};
+              `}
+          renderInput={({getInputProps}) => (
+            <InputWrapper>
+              <Input
+                autoFocus
+                {...getInputProps({
+                  type: 'text',
+                  placeholder: t('Search for documentation, FAQs, blog posts...'),
+                })}
+              />
+            </InputWrapper>
+          )}
+          resultFooter={<Hook name="help-modal:footer" {...{organization, closeModal}} />}
+        />
+      )}
+    </ClassNames>
+  </Body>
+);
+
+const InputWrapper = styled('div')`
+  padding: 2px;
+`;
+
+const Input = styled('input')`
+  width: 100%;
+  padding: 8px;
+  border: none;
+  border-radius: 8px;
+  outline: none;
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+export const modalCss = css`
+  .modal-content {
+    padding: 0;
+  }
+`;
+
+export default HelpSearchModal;
